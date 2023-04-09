5d6506a1e02d8f3094e3ee3b4b8d83bd6e15e8f5
Sunny <sunnysideupdowntown@gmail.com>
2023-04-10 00:53:22 +0900

    ViewController에서 CustomCameraViewController로 연결

--- CustomCameraSample/ViewController.swift
+++ CustomCameraSample/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        
+        // 카메라 화면 여는 버튼 추가
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Camera", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(cameraButtonTapped), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
-
-
+    
+    @objc private func cameraButtonTapped() {
+        
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            
+        // 권한 O -> 카메라 화면 보여줌
+        case .authorized:
+            let cameraViewController = CustomCameraViewController()
+            cameraViewController.modalPresentationStyle = .overFullScreen
+            cameraViewController.modalTransitionStyle = .coverVertical
+            self.present(cameraViewController, animated: true)
+            
+        // 결정 X -> 권한 요청
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) {_ in }
+        
+        default:
+            break
+        }
+    }
 }
 
